4c9d597c87e318dbb2badd6411cb14ef95b7a4c4
Riley Bathurst <riley@rileybathurst.com>
2025-09-10 16:12:08 -0700

    seo and meta

--- src/components/meta.tsx
+++ src/components/meta.tsx
@@ -6,7 +6,7 @@ const formatted = match ? `(${match[1]}) ${match[2]}-${match[3]}` : "";
 export const Meta = {
   title: "Jarrod Semmens Window Cleaning",
   description:
-    "Jarrod Semmens Window Cleaning is a locally owned and operated business that provides professional window cleaning services to residential and commercial customers in Tahoe City, CA.",
+    "Jarrod Semmens Window Cleaning can supply you with a thorough washing of your windows inside and out. From our high-quality, professional staff to our effective cleaning techniques, you'll get the satisfaction of gleaming, sparkling results from a locally owned and operated company.",
   email: "service@jarrodsemmenswindowcleaning.com",
   WebSite: "https://www.jarrodsemmenswindowcleaning.com",
   phoneRaw: phone,
@@ -25,4 +25,47 @@ export const Meta = {
     addressRegion: "CA",
     postalCode: 96140,
   },
+
+  keywords: [
+    "window cleaning",
+    "window washer",
+    "window washing",
+    "residential window cleaning",
+    "commercial window cleaning",
+    "screen cleaning",
+    "Window cleaning Tahoe City",
+    "Professional window washing CA",
+    "Residential window cleaning Lake Tahoe",
+    "Commercial window cleaning Tahoe area",
+    "Broken screen replacement Tahoe City"
+  ],
+
+  areaServed: [
+    "Tahoe City",
+    "Meeks Bay",
+    "Tahoma",
+    "Homewood",
+    "Tahoe Pines",
+    "Timberland",
+    "Dollar Point",
+    "Ridgewood",
+    "Carnelian Bay",
+    "Kings Beach",
+    "Incline Village",
+    "Alpine Meadows",
+    "Olympic Valley",
+    "Truckee",
+    "Northstar",
+    "Donner Lake",
+    "Glenshire",
+    "South Lake Tahoe",
+  ],
+
+  image: 'https://jarrodsemmenswindowcleaning.s3.us-west-1.amazonaws.com/jswc-og_image.jpg',
+  openingHours: 'Mo, Tu, We, Th, Fr 09:00-17:00',
+  faxNumber: '(530) 581-1981',
+  slogan: 'We’re your local number one source in Window Cleaning in Tahoe City and surrounding areas.'
+
+  // https://schema.org/logo
+  // https://schema.org/image
 };
